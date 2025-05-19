23613e6062a0f13ac3bcb589c85e3933c169b4a9
alikashipazha <53570839+alikashipazha@users.noreply.github.com>
2025-05-19 19:28:23 +0330

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Camera.h"
+#include "Monitor.h"
 
 int main() {
     Camera cam;
@@ -9,7 +10,30 @@ int main() {
 
     while (true) {
         cam.update();
+		if(cam.getSuccess()) {
+			// مسیر پوشه ویدیو و فایل ویدیو
+			string videosDir = "/home/salam-robo/test/video";
+			string videoFile = videosDir + "/first.mp4";
 
+			Monitor monitor(videosDir);
+
+			monitor.setVideo(videoFile);
+			monitor.setTask(Monitor::Task::PLAY_VIDEO);
+
+			cout << "Starting video playback: " << videoFile << endl;
+
+			// چک کردن وضعیت پخش هر 500 میلی‌ثانیه
+			while (true) {
+				monitor.update();
+
+				if (monitor.getVideoFinished()) {
+					cout << "Video playback finished." << endl;
+					break;
+				}
+
+				this_thread::sleep_for(chrono::milliseconds(500));
+			}
+		}
         // خروج با کلید ESC
         if (cv::waitKey(1) == 27) {
             std::cout << "ESC pressed. Turning off and exiting...\n";
