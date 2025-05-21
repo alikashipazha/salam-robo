c01cf50d4f3cf3a8049b883be88bcd30fd6bd8d5
alikashipazha <53570839+alikashipazha@users.noreply.github.com>
2025-05-21 17:41:49 +0330

    Add files via upload

--- sonarTest2.cpp
+++ sonarTest2.cpp
@@ -15,13 +15,30 @@ int main() {
     while (true) {
         // sonar.update();
 
-        if (sonar.getSuccess()) {
-            std::cout << "success" << std::endl;
-        } else if (sonar.getFail()) {
-            std::cout << "fail" << std::endl;
+        // if (sonar.getSuccess()) {
+        //     std::cout << "success" << std::endl;
+        // } else if (sonar.getFail()) {
+        //     std::cout << "fail" << std::endl;
+        // }
+
+        // delay(10); // صبر 200 میلی‌ثانیه برای کاهش سرعت چاپ
+        while (true) {
+            bool success = sonar.getSuccess();
+            bool fail = sonar.getFail();
+
+            if (success && !fail) {
+                std::cout << "success" << std::endl;
+            } else if (fail && !success) {
+                std::cout << "fail" << std::endl;
+            } else if (!success && !fail) {
+                std::cout << "no signal" << std::endl;
+            } else {
+                std::cout << "both signals active! check wiring!" << std::endl;
+            }
+
+            delay(5);
         }
 
-        delay(10); // صبر 200 میلی‌ثانیه برای کاهش سرعت چاپ
     }
 
     return 0;
